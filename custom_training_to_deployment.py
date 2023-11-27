a9ca93cfb94fce0ba290dc2b7a8c0c3c8a096449
DixitAkriti <104968035+DixitAkriti@users.noreply.github.com>
2023-11-27 13:22:20 +0530

    Update custom_training_to_deployment.py

--- custom_training_to_deployment.py
+++ custom_training_to_deployment.py
@@ -96,6 +96,6 @@ job = aip.PipelineJob(
     display_name=DISPLAY_NAME,
     template_path="tabular_regression_pipeline.json",
     pipeline_root=PIPELINE_ROOT,
-    enable_caching=False,
+    enable_caching=True,
 )
 job.run(service_account="vertex-sa@sandbox-dev-dbg.iam.gserviceaccount.com")
